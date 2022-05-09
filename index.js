7de548b1da1bb4688547d97f07ba41c1b2ebee6e
amr303 <amr.abdulmatin@gmail.com>
2022-05-09 18:27:40 +0600

    service data load

--- index.js
+++ index.js
@@ -10,34 +10,38 @@ app.use(cors());
 app.use(express.json());
 
 
-// user: dbuser1
-// password: bYwZDiGaQiMl80Ni
 
 
-
-const uri = "mongodb+srv://dbuser1:bYwZDiGaQiMl80Ni@cluster0.h7ggo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.h7ggo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
-    try{
-        await client.connect();
-        const userCollection = client.db("laptopsource").collection("user");
-        const user = { name: "Abdul", email: 'abdul@gmail.com' }
-        const result = await userCollection.insertOne(user);
-        console.log(`user inserted with id: ${result.insertedId} `);
+    try {
+
     }
-    finally{
-        // await client.close();
+    finally {
+        await client.connect();
+        const serviceCollection = client.db('laptopSource').collection('service');
+
+        app.get('/service', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        })
     }
 }
-
 run().catch(console.dir);
 
-app.get('/',(req,res)=>{
+
+
+
+
+app.get('/', (req, res) => {
     res.send('Running sevver');
 });
 
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log('Listening to port ', port);
 });
